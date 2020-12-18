a5bbd5fac1e1ffd43bad78bcc2e94e9067231711
Brouse13 <unaiunai2004@gmail.com>
2020-12-18 20:38:34 +0100

    ExperimentalShieldEncoding
    - Untested method to encode and decode the permission

--- src/main/java/net/noobsters/kern/paper/objects/encoder/ShieldEncoder.java
+++ src/main/java/net/noobsters/kern/paper/objects/encoder/ShieldEncoder.java
@@ -1,5 +1,6 @@
 package net.noobsters.kern.paper.objects.encoder;
 
+import net.noobsters.kern.paper.shield.ShieldPattern;
 import org.bukkit.DyeColor;
 import org.bukkit.block.banner.Pattern;
 import org.bukkit.block.banner.PatternType;
@@ -7,7 +8,7 @@ import org.bukkit.block.banner.PatternType;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ShieldEncoder{
+public class ShieldEncoder {
     public DyeColor[] dyeColors() {
         return DyeColor.values();
     }
@@ -16,52 +17,67 @@ public class ShieldEncoder{
         return PatternType.values();
     }
 
-    public int encode(Pattern pattern) {
+    public int encode(DyeColor background, Pattern pattern) {
         StringBuilder encode = new StringBuilder();
-        for (int i = 0; i < dyeColors().length; i++) {
-            if (dyeColors()[i].equals(pattern.getColor())) {
-                if (i > 10)
-                    encode.append(0);
-                encode.append(i);
-            }
-        }
-
-        for (int i = 0; i < patternTypes().length; i++) {
-            if (patternTypes()[i].equals(pattern.getPattern())) {
-                if (i > 10)
-                    encode.append(0);
-                encode.append(i);
-            }
-        }
-        return Integer.parseInt(encode.toString());
+        encode.append(encodeColor(background));
+        encode.append(encodePattern(pattern));
+        return  Integer.parseInt(encode.toString());
     }
 
-    public int encode(List<Pattern> patterns) {
+    public int encode(DyeColor background, List<Pattern> patterns) {
         StringBuilder encode = new StringBuilder();
+        encode.append(encodeColor(background));
+
         for (Pattern pattern : patterns)
-            encode.append(encode(pattern));
+            encode.append(encodePattern(pattern));
+
         return Integer.parseInt(encode.toString());
     }
 
-    public List<Pattern> decode(int encode) {
-        List<Pattern> decoded_packet = new ArrayList<>();
+    public ShieldPattern decode(int encode) {
+        List<Pattern> decoded_patterns = new ArrayList<>();
         String[] encode_packets = new String[encode / 4];
-        if (encode % 4 != 0)
+        if (encode % 4 == 2)
             throw new IllegalArgumentException("Encode format exception: "+ encode);
 
-        for (int i = 0; i < encode_packets.length; i++) {
+
+        for (int i = 2; i < encode_packets.length; i++) {
             encode_packets[i] = String.valueOf(encode).substring(i, i+4);
         }
 
         for (String packet : encode_packets) {
-            decoded_packet.add(new Pattern(
+            decoded_patterns.add(new Pattern(
                     dyeColors()[Integer.parseInt(packet.substring(0, 2))],
                     patternTypes()[Integer.parseInt(packet.substring(2))]
             ));
         }
-        return decoded_packet;
+        return new ShieldPattern(null,
+                dyeColors()[Integer.parseInt(String.valueOf(encode).substring(0, 2))],
+                decoded_patterns);
     }
 
+    private int encodeColor(DyeColor color) {
+        StringBuilder encode = new StringBuilder();
+        for (int i = 0; i < dyeColors().length; i++) {
+            if (dyeColors()[i].equals(color)) {
+                if (i > 10) encode.append(0);
+                encode.append(i);
+            }
+        }
+        return Integer.parseInt(encode.toString());
+    }
 
+    private int encodePattern(Pattern pattern) {
+        StringBuilder encode = new StringBuilder();
+        encode.append(encodeColor(pattern.getColor()));
+
+        for (int i = 0; i < patternTypes().length; i++) {
+            if (patternTypes()[i].equals(pattern.getPattern())) {
+                if (i > 10) encode.append(0);
+                encode.append(i);
+            }
+        }
+        return Integer.parseInt(encode.toString());
+    }
 }
 
