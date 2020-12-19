48f468f6df731ce4bc2d343dd5eeb6a54ae081b6
Brouse13 <unaiunai2004@gmail.com>
2020-12-19 16:14:22 +0100

    ShieldEncoder working
    -Shield Encoder is now ready

--- src/main/java/net/noobsters/kern/paper/objects/encoder/ShieldEncoder.java
+++ src/main/java/net/noobsters/kern/paper/objects/encoder/ShieldEncoder.java
@@ -9,75 +9,74 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ShieldEncoder {
-    public DyeColor[] dyeColors() {
-        return DyeColor.values();
-    }
+    private static DyeColor[] dyeColors = DyeColor.values();
+    private static PatternType[] patternTypes = PatternType.values();
 
-    public PatternType[] patternTypes() {
-        return PatternType.values();
-    }
-
-    public int encode(DyeColor background, Pattern pattern) {
+    public static String encode(DyeColor background, Pattern pattern) {
         StringBuilder encode = new StringBuilder();
         encode.append(encodeColor(background));
+
         encode.append(encodePattern(pattern));
-        return  Integer.parseInt(encode.toString());
+        return encode.toString();
     }
 
-    public int encode(DyeColor background, List<Pattern> patterns) {
+    public static String encode(DyeColor background, List<Pattern> patterns) {
         StringBuilder encode = new StringBuilder();
         encode.append(encodeColor(background));
 
         for (Pattern pattern : patterns)
             encode.append(encodePattern(pattern));
 
-        return Integer.parseInt(encode.toString());
+        return encode.toString();
     }
 
-    public ShieldPattern decode(int encode) {
+    public static ShieldPattern decode(String encode) {//15 1135 731
         List<Pattern> decoded_patterns = new ArrayList<>();
-        String[] encode_packets = new String[encode / 4];
-        if (encode % 4 == 2)
+        String[] encode_packets = new String[String.valueOf(encode).length() / 4];
+
+        if (String.valueOf(encode).length() % 4 != 2)
             throw new IllegalArgumentException("Encode format exception: "+ encode);
 
-
-        for (int i = 2; i < encode_packets.length; i++) {
-            encode_packets[i] = String.valueOf(encode).substring(i, i+4);
+        for (int i = 0; i < encode_packets.length; i++) {
+            encode_packets[i] = String.valueOf(encode).substring(i*3+i+2, i*3+i+6);
+            System.out.println(encode_packets[i]);
         }
 
         for (String packet : encode_packets) {
             decoded_patterns.add(new Pattern(
-                    dyeColors()[Integer.parseInt(packet.substring(0, 2))],
-                    patternTypes()[Integer.parseInt(packet.substring(2))]
+                    dyeColors[Integer.parseInt(packet.substring(0, 2))],
+                    patternTypes[Integer.parseInt(packet.substring(2))]
             ));
+
         }
         return new ShieldPattern(null,
-                dyeColors()[Integer.parseInt(String.valueOf(encode).substring(0, 2))],
+                dyeColors[Integer.parseInt(String.valueOf(encode).substring(0, 2))],
                 decoded_patterns);
     }
 
-    private int encodeColor(DyeColor color) {
+    private static String encodeColor(DyeColor color) {
         StringBuilder encode = new StringBuilder();
-        for (int i = 0; i < dyeColors().length; i++) {
-            if (dyeColors()[i].equals(color)) {
-                if (i > 10) encode.append(0);
+        for (int i = 0; i < dyeColors.length; i++) {
+            if (dyeColors[i].equals(color)) {
+                if (i < 10)
+                    encode.append("0");
                 encode.append(i);
             }
         }
-        return Integer.parseInt(encode.toString());
+        return encode.toString();
     }
 
-    private int encodePattern(Pattern pattern) {
+    private static String encodePattern(Pattern pattern) {
         StringBuilder encode = new StringBuilder();
         encode.append(encodeColor(pattern.getColor()));
 
-        for (int i = 0; i < patternTypes().length; i++) {
-            if (patternTypes()[i].equals(pattern.getPattern())) {
-                if (i > 10) encode.append(0);
+        for (int i = 0; i < patternTypes.length; i++) {
+            if (patternTypes[i].equals(pattern.getPattern())) {
+                if (i < 10) encode.append("0");
                 encode.append(i);
             }
         }
-        return Integer.parseInt(encode.toString());
+        return encode.toString();
     }
 }
 
