97e133b2553d1cc993ea6241f5599420e0361592
InfinityZ25 <henixceo@gmail.com>
2020-12-19 00:04:42 -0500

    Functional Example

--- src/main/java/net/noobsters/kern/paper/databases/listeners/DatabaseListener.java
+++ src/main/java/net/noobsters/kern/paper/databases/listeners/DatabaseListener.java
@@ -1,14 +1,18 @@
 package net.noobsters.kern.paper.databases.listeners;
 
-import java.util.Map;
-import java.util.Random;
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Updates.addToSet;
+import static com.mongodb.client.model.Updates.inc;
 
-import com.mongodb.BasicDBObject;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.bson.Document;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -17,21 +21,40 @@ import net.noobsters.kern.paper.databases.DatabaseManager;
 @RequiredArgsConstructor(staticName = "of")
 public class DatabaseListener implements Listener {
     private @NonNull DatabaseManager dbManager;
+    private HashMap<String, Long> connectionTime = new HashMap<>();
 
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
         final var player = e.getPlayer();
         var doc = dbManager.isInCollection(player.getUniqueId());
+        connectionTime.put(player.getUniqueId().toString(), System.currentTimeMillis());
 
         if (doc != null) {
-            System.out.println("reading from db");
-            dbManager.getCollection().updateOne(new BasicDBObject().append("_id", player.getUniqueId()),
-                    of(Map.of("$inc", Map.of("user_data.played_time", new Random().nextLong()))));
-            System.out.println(doc.toJson());
+            Bukkit.broadcastMessage(doc.toJson());
+            var ip = player.getAddress().getHostName();
+            var time = doc.get("user_data", Document.class).getLong("played_time");
+            dbManager.getCollection().updateOne(eq("_id", player.getUniqueId()), addToSet("user_data.known_ips", ip));
+            player.sendMessage("Welcome back. You have " + Math.round(time / 1000) + " seconds played.");
+
         } else {
-            System.out.println("Adding to db");
+            player.sendMessage("Welcome to Noobsters! Enjoy your stay!");
             dbManager.getCollection().insertOne(of(Map.of("_id", player.getUniqueId(), "user_data",
                     of(Map.of("name", player.getName(), "first_join", System.currentTimeMillis(), "played_time", 0)))));
+        }
+
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent e) {
+        final var player = e.getPlayer();
+        var doc = dbManager.isInCollection(player.getUniqueId());
+
+        if (doc != null) {
+            // Update time later
+            var time_diff = System.currentTimeMillis() - connectionTime.remove(player.getUniqueId().toString());
+            dbManager.getCollection().updateOne(eq("_id", player.getUniqueId()),
+                    inc("user_data.played_time", time_diff));
+
         }
 
     }
