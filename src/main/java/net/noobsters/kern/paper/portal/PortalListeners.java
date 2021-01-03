dc64b0f2cc8d2b5654eb137eb95f7387325f9848
InfinityZ25 <henixceo@gmail.com>
2021-01-02 23:07:18 -0500

    Changes on algo, almost ready

--- src/main/java/net/noobsters/kern/paper/portal/PortalListeners.java
+++ src/main/java/net/noobsters/kern/paper/portal/PortalListeners.java
@@ -1,7 +1,6 @@
 package net.noobsters.kern.paper.portal;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashMap;
 
 import org.bukkit.Bukkit;
@@ -11,7 +10,9 @@ import org.bukkit.World;
 import org.bukkit.World.Environment;
 import org.bukkit.WorldCreator;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -62,65 +63,147 @@ public class PortalListeners extends BaseCommand implements Listener {
 
     }
 
-    @Subcommand("list")
-    public void portalsList(CommandSender sender) {
-        if (p.isEmpty()) {
-            sender.sendMessage("No players in list");
-        } else {
-
-            p.forEach(all -> {
-                sender.sendMessage(all);
-            });
-        }
-    }
-
     @Subcommand("clear")
     public void portalsListClear(CommandSender sender) {
         p.clear();
     }
 
     @Subcommand("pc")
-    public void onPortalCreate(final Player player) {
-        createPortal(player.getLocation().getBlock());
-    }
-
-    @EventHandler
-    public void exit(EntityPortalExitEvent e) {
-        Bukkit.broadcastMessage("exited");
-
-    }
-
-    @EventHandler
-    public void onMove(PlayerMoveEvent e) {
-        if (e.getFrom().distance(e.getTo()) > 0 && portalMap.containsKey(e.getPlayer().getUniqueId().toString())
-                && e.getTo().getBlock().getType() != Material.NETHER_PORTAL) {
-            portalMap.remove(e.getPlayer().getUniqueId().toString());
-        }
-
+    @CommandCompletion("@worlds <Integer>")
+    public void onPortalCreate(final Player player, World world, int ratio) {
+        var initial_time = System.currentTimeMillis();
+        travelDimensions(player, player.getLocation(), world, ratio);
+        Bukkit.broadcastMessage("Took " + (System.currentTimeMillis() - initial_time) + "ms to teleport.");
     }
 
     /**
      * NETHER ALGO STARTS
      */
 
+    // Portal Object
     @Data
     @AllArgsConstructor(staticName = "of")
     public static class Portal {
-        ArrayList<Block> frame;
-        ArrayList<Block> portal_blocks;
-        
-        public static Portal createPortal(){
+        Block[][] portal_blocks;
+        Block[][][] frame;
+        Block[][][] air_blocks;
 
-            return of(null, null);
+        public static Portal createPortal(Location loc) {
+            final var b = loc.getBlock();
+            final var portalBlocks = new Block[4][5];
+            final var frameBlocks = new Block[4][5][3];
+            final var airBlocks = new Block[4][5][3];
 
+            for (int x = 0; x < 4; x++) {
+                for (int y = 0; y < 5; y++) {
+                    for (int z = 0; z < 3; z++) {
+                        if (z == 1) {
+                            if ((x == 1 || x == 2) && y >= 1 && y <= 3) {
+                                var relative = b.getRelative(x, y, z);
+                                relative.setType(Material.AIR);
+                                portalBlocks[x][y] = relative;
+                                continue;
+                            }
+                            var relative = b.getRelative(x, y, z);
+                            relative.setType(Material.OBSIDIAN);
+                            frameBlocks[x][y][z] = relative;
+                        } else if (y == 0) {
+
+                            var relative = b.getRelative(x, y, z);
+                            relative.setType(Material.OBSIDIAN);
+                            frameBlocks[x][y][z] = relative;
+                        } else {
+                            var relative = b.getRelative(x, y, z);
+                            relative.setType(Material.AIR);
+                            airBlocks[x][y][z] = relative;
+                        }
+
+                    }
+                }
+
+            }
+
+            portalBlocks[1][1].setType(Material.FIRE);
+
+            return of(portalBlocks, frameBlocks, airBlocks);
+        }
+
+        public Location getTeleportLocation() {
+            var left = portal_blocks[1][2];
+            var right = portal_blocks[2][2];
+            return new Location(left.getWorld(), (left.getX() + right.getX()) / 2, left.getY(),
+                    (left.getZ() + right.getZ()) / 2);
         }
     }
-    
+
+    // Travel function
+    public boolean travelDimensions(Entity entity, Location from, World target, int ratio) {
+        final var ratioed_target = new Location(target, from.getX() * ratio, from.getY(), from.getZ() * ratio);
+
+        var min = ratioed_target.clone().add(-64, 0, -64);
+        min.setY(30);
+        var max = ratioed_target.clone().add(64, 0, 64);
+        max.setY(100);
+
+        var cuboid_matrix = new ArrayList<Block>();
+        var optimal = new ArrayList<Block>();
+
+        for (int x = min.getBlockX(); x <= max.getBlockX(); x++) {
+            for (int y = min.getBlockY(); y <= max.getBlockY()
+                    && y <= Math.min(max.getBlockY(), target.getMaxHeight()); y++) {
+                for (int z = max.getBlockZ(); z >= min.getBlockZ(); z--) {
+                    var block = target.getBlockAt(x, y, z);
+                    if (block.getType() == Material.NETHER_PORTAL) {
+                        entity.teleport(block.getLocation());
+                        cuboid_matrix.clear();
+                        cuboid_matrix = null;
+
+                        Bukkit.broadcastMessage("Found existing portal");
+                        return true;
+                    }
+                    if (block.getType() != Material.AIR && block.getRelative(BlockFace.UP).getType() == Material.AIR) {
+                        optimal.add(block);
+                    }
+                    cuboid_matrix.add(block);
+                }
+            }
+        }
+        // Find Closest one
+        double last_distance = Integer.MAX_VALUE;
+        Location closest_loc = null;
+        // Loop and replace
+        for (var optimal_loc : optimal) {
+            var d = optimal_loc.getLocation().distance(ratioed_target);
+            if (d < last_distance) {
+                last_distance = d;
+                closest_loc = optimal_loc.getLocation();
+            }
+        }
+        // Create portal return true
+        if (closest_loc != null) {
+            Bukkit.broadcastMessage("Creating new portal");
+            var portal = Portal.createPortal(closest_loc);
+            entity.teleport(portal.getTeleportLocation());
+            return true;
+        } else {
+            Bukkit.broadcastMessage("Creating new portal at 1:1");
+            var portal = Portal.createPortal(ratioed_target);
+            entity.teleport(portal.getTeleportLocation());
+        }
+
+        return false;
+    }
+
+    public void travelDimensions(Entity entity, Location from, World target) {
+        travelDimensions(entity, from, target, 1);
+    }
+
     /**
      * NETHER ALGO ENDS
      */
 
-    ArrayList<String> p = new ArrayList<>();
+    HashMap<Player, Integer> p = new HashMap<>();
+    HashMap<Player, Location> portalLocation = new HashMap<>();
     HashMap<Cuboid, Location> locations = new HashMap<>();
 
     @EventHandler
@@ -128,112 +211,63 @@ public class PortalListeners extends BaseCommand implements Listener {
         if (e.getEntityType() != EntityType.PLAYER)
             return;
         final var player = (Player) e.getEntity();
-
-        if (p.contains(player.getUniqueId().toString())) {
+        final var cd = p.get(player);
+        portalLocation.put(player, player.getLocation());
+        if (cd != null) {
             return;
         }
-        var oneByThreematrix = new int[][][] {};
 
-        p.add(player.getUniqueId().toString());
-
-        final var loc = player.getLocation();
-        if (loc.getWorld().getEnvironment() == Environment.NORMAL) {
-            var nether = Bukkit.getWorld(loc.getWorld().getName() + "_nether");
-            if (nether != null) {
-                var ratioedLocation = of(nether, Math.floor(loc.getX()), loc.getY(), Math.floor(loc.getZ()));
-                var min = ratioedLocation.clone().add(-64, 0, -64);
-                min.setY(100);
-                var max = ratioedLocation.clone().add(64, 0, 64);
-                max.setY(30);
-                var cuboid = new Cuboid(min, max);
-                var list = new ArrayList<Block>();
-
-                var bl = cuboid.getBlocks();
-                Location teleport = null;
-
-                for (var c : bl) {
-                    var type = c.getType();
-                    var l = c.getLocation();
-                    if (type == Material.NETHER_PORTAL) {
-                        Bukkit.broadcastMessage("portal_found");
-                        oneByThreematrix[0][1][2] = 1;
-                        teleport = l;
-                        break;
-                    } else if (type == Material.AIR) {
-                        list.add(c);
-                    }
-                }
-
-                if (teleport != null) {
-                    final var ftp = teleport;
-
-                    player.teleport(ftp);
-                    p.remove(player.getUniqueId().toString());
-                    Bukkit.broadcastMessage("found_teleport\n" + ftp.toString());
-
-                } else {
-                    // make ir higher
-                    teleport = list.get(0).getLocation();
-                    list.stream().max(Comparator.comparingInt(Block::getY)).ifPresent(a -> {
-
-                        final var ftp = createPortal(a);
-
-                        player.teleport(ftp);
-                        p.remove(player.getUniqueId().toString());
-                        Bukkit.broadcastMessage("random_teleport\n" + ftp.toString());
-                    });
-
-                    // TODO: Create portal as an object with the structure positions easy to access.
-                }
-
-            }
-
-        } else {
-            p.remove(player.getUniqueId().toString());
-        }
+        Bukkit.broadcastMessage("Attempting.");
+        travel(player, 50);
 
     }
 
-    private Location createPortal(Block b) {
-
-        Block fireBlock = null;
-
-        for (int x = 0; x < 4; x++) {
-            for (int y = 0; y < 5; y++) {
-                for (int z = 0; z < 3; z++) {
-                    if (z == 1) {
-                        if ((x == 1 || x == 2) && y >= 1 && y <= 3) {
-                            var relative = b.getRelative(x, y, z);
-                            if (fireBlock == null)
-                                fireBlock = relative;
-                            relative.setType(Material.AIR);
-                            continue;
-                        }
-                        b.getRelative(x, y, z).setType(Material.OBSIDIAN);
-                    } else if (y == 0) {
-
-                        b.getRelative(x, y, z).setType(Material.OBSIDIAN);
-                    } else {
-                        b.getRelative(x, y, z).setType(Material.AIR);
-
-                    }
-
-                }
-            }
-
+    public void travel(Player player, int cd) {
+        if (!p.containsKey(player)) {
+            p.put(player, cd);
         }
-        fireBlock.setType(Material.FIRE);
-        return fireBlock.getLocation();
+        Bukkit.getScheduler().runTaskLater(instance, () -> {
+            var block = player.getLocation();
+            var distance = block.distance(portalLocation.get(player));
+            if (distance == 0.0) {
+                if (cd > 0) {
+                    travel(player, (cd - 1));
+                } else {
+                    var world = player.getWorld();
+                    if (world.getEnvironment() == Environment.NORMAL) {
+                        var nether = Bukkit.getWorld(world.getName() + "_nether");
+                        if (nether != null) {
+                            travelDimensions(player, player.getLocation(), nether);
+                        } else {
+                            player.sendMessage("No nether world available");
+                        }
+                        Bukkit.getScheduler().runTaskLater(instance, () -> {
+                            p.remove(player);
+                        }, 40);
+                    } else if (world.getEnvironment() == Environment.NETHER) {
+                        var overworld = Bukkit.getWorld(world.getName().replace("_nether", ""));
+                        if (overworld != null) {
+                            travelDimensions(player, player.getLocation(), overworld);
+                        } else {
+                            player.sendMessage("No overworld available");
+                        }
+                        Bukkit.getScheduler().runTaskLater(instance, () -> {
+                            p.remove(player);
+                        }, 40);
+                    }
+                }
+
+            } else {
+                Bukkit.broadcastMessage("Cancelled");
+                p.remove(player);
+            }
+        }, 1L);
     }
 
     @EventHandler
     public void onLeafDecay(LeavesDecayEvent e) {
         e.setCancelled(true);
 
-    }
-
-    private static Location of(World world, double x, double y, double z) {
-        return new Location(world, x, y, z);
     }
 
     /**
