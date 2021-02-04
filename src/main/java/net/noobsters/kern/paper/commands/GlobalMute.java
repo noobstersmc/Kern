a6f8d3861c7b8796104b4c3c2e6ae51680703f77
AleIV <alejandrocorrales0201@gmail.com>
2021-02-04 17:39:10 -0600

    update

--- src/main/java/net/noobsters/kern/paper/commands/GlobalMute.java
+++ src/main/java/net/noobsters/kern/paper/commands/GlobalMute.java
@@ -7,6 +7,7 @@ import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.CommandPermission;
 import co.aikar.commands.annotation.Default;
+import co.aikar.commands.annotation.Optional;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import net.md_5.bungee.api.ChatColor;
@@ -19,9 +20,13 @@ public class GlobalMute extends BaseCommand {
     private @NonNull Kern instance;
 
     @Default
-    public void onCommand(CommandSender sender) {
+    public void onCommand(CommandSender sender, @Optional Boolean bool) {
         var chat = instance.getChatManager();
-        chat.setGlobalmute(!chat.isGlobalmute());
+        if(bool == null){
+            chat.setGlobalmute(!chat.isGlobalmute());
+        }else{
+            chat.setGlobalmute(bool);
+        }
 
         Bukkit.broadcastMessage(ChatColor.of("#2be49c")
                 + (chat.isGlobalmute() ? "Globalmute Enabled." : "Globalmute Disabled."));
