bf0b90ec4fb7c71af961e4aede5626f9e1d6f2a1
Juan Cedeno <henixceo@gmail.com>
2021-04-14 15:02:45 -0400

    fixed grammar

--- src/main/java/net/noobsters/kern/paper/punishments/gui/PunizioneGui.java
+++ src/main/java/net/noobsters/kern/paper/punishments/gui/PunizioneGui.java
@@ -16,7 +16,7 @@ public class PunizioneGui {
 
         //TODO: BAMOS
         var teaming = new ItemBuilder(Material.LEAD).name(ChatColor.YELLOW + "Teaming")
-        .addLore(ChatColor.WHITE + "+1 penaltie point", ChatColor.GRAY + "Ban " + penalties*12 + " hours").build();
+        .addLore(ChatColor.WHITE + "+1 penalty point", ChatColor.GRAY + "Ban " + penalties*12 + " hours").build();
         gui.setItem(0, teaming, click->{
             
             //ban penalties x12 hours
@@ -24,20 +24,20 @@ public class PunizioneGui {
         });
 
         var spam = new ItemBuilder(Material.MAP).name(ChatColor.YELLOW + "Spam / Flood")
-        .addLore(ChatColor.WHITE + "+1 penaltie point", ChatColor.GRAY + "Mute " + penalties*2 + " hours").build();
+        .addLore(ChatColor.WHITE + "+1 penalty point", ChatColor.GRAY + "Mute " + penalties*2 + " hours").build();
         gui.setItem(2, spam, click->{
             //mute penalties x 2 hours
             //add penaltie points
         });
 
         var inappropriateLanguage = new ItemBuilder(Material.POISONOUS_POTATO).name(ChatColor.YELLOW + "Inappropriate Language")
-        .addLore(ChatColor.WHITE + "+1 penaltie point", ChatColor.GRAY + "Mute " + penalties*2 + " hours").build();
+        .addLore(ChatColor.WHITE + "+1 penalty point", ChatColor.GRAY + "Mute " + penalties*2 + " hours").build();
         gui.setItem(3, inappropriateLanguage, click->{
             //mute penalties x 2 hour
         });
 
         var sports = new ItemBuilder(Material.BARRIER).name(ChatColor.GOLD + "Bad Sportsmanship")
-        .addLore(ChatColor.WHITE + "+3 penaltie points", ChatColor.GRAY + (penalties >= 12 ? "Ban " : "Mute ") + penalties + " days").build();
+        .addLore(ChatColor.WHITE + "+3 penalty points", ChatColor.GRAY + (penalties >= 12 ? "Ban " : "Mute ") + penalties + " days").build();
         gui.setItem(5, sports, click->{
             if(penalties >= 12){
                 //is a ban
@@ -51,7 +51,7 @@ public class PunizioneGui {
         });
 
         var hate = new ItemBuilder(Material.WITHER_ROSE).name(ChatColor.GOLD + "Hate Speech")
-        .addLore(ChatColor.WHITE + "+3 penaltie points", ChatColor.GRAY + (penalties >= 12 ? "Ban " : "Mute ") + penalties + " days").build();
+        .addLore(ChatColor.WHITE + "+3 penalty points", ChatColor.GRAY + (penalties >= 12 ? "Ban " : "Mute ") + penalties + " days").build();
         gui.setItem(6, hate, click->{
             if(penalties >= 12){
                 //is a ban
@@ -65,7 +65,7 @@ public class PunizioneGui {
         });
 
         var hack = new ItemBuilder(Material.NETHERITE_SCRAP).name(ChatColor.DARK_RED + "Hacked Client")
-        .addLore(ChatColor.WHITE + "+12 penaltie points", ChatColor.GRAY + "Undefined ban").build();
+        .addLore(ChatColor.WHITE + "+12 penalty points", ChatColor.GRAY + "Undefined ban").build();
         gui.setItem(8, hack, click->{
             //ban perma
             //add penaltie points
